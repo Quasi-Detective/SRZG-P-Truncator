d45af23c5be9d212e27b68a7eacdd97c14225389
Noëlle McHenry <74688135+Quasi-Detective@users.noreply.github.com>
2024-01-19 17:00:09 -0600

    Add files via upload

---
+++ pack_truncate.py
@@ -0,0 +1,75 @@
+# ZG .P Truncator (for Python 3.8) by Quasi-Detective
+# 2024.01.19
+# Aims to properly truncate the 'OOCH' archives (.P and no extension) from Sonic Riders: Zero Gravity into files that can then be decrypted to .pack format using https://github.com/romhack/sonic_riders_lzss
+# Based off of the method by moester_ in https://www.youtube.com/watch?v=q65xyi4Rn-c
+# Place this script as a .py file into the folder with files you want to scan/truncate. Please make backups of the files if you want to revert any changes or in case there is an issue!
+
+import os
+import binascii
+import sys
+
+for fileO in os.listdir('.'):
+	if not fileO.endswith(".new3"):
+		with open(fileO, "rb") as f:
+			print("File: %s" % str(fileO))
+			headerB = binascii.hexlify(f.readline(4))
+			if str(headerB.decode('utf8')) == '4f4f4348' and not fileO.endswith(".new"):
+				print("OOCH header found! Proceeding.")
+				#Do the thing.
+				newFile = open(fileO + ".new1", "wb")
+				f.seek(0)
+				newFile.write(f.read(16))
+				f.seek(2016)
+				newFile.write(f.read(os.path.getsize(fileO)))
+				f.close()
+				newFile.close()
+				#os.rename(fileO + ".new1", fileO)
+				newFileB = open(fileO + ".new1", "rb")
+				newFile2 = open(fileO + ".new2", "wb")
+				newFileB.seek(0)
+				newFile2.write(newFileB.read(40))
+				newFileB.seek(48)
+				newFile2.write(newFileB.read(os.path.getsize(fileO + ".new1")))
+				newFileB.close()
+				newFile2.close()
+				newFile2B = open(fileO + ".new2", "rb")
+				newFile3 = open(fileO + ".new3", "wb")
+				newFile3.write(b'\x80\x00\x00\x01')
+				newFile2B.seek(12)
+				newFile3.write(newFile2B.read(os.path.getsize(fileO + ".new2")))
+				newFile2B.close()
+				newFile3.close()
+				os.remove(fileO + ".new1")
+				os.remove(fileO + ".new2")
+				os.remove(fileO)
+				os.rename(fileO + ".new3", fileO)
+				
+			else:
+				print("Header '%s' is incorrect. Skipping." % str(headerB.decode('utf8')))
+				#Don't do the thing.
+				f.close()
+
+            #f.seek(-8, os.SEEK_END)
+            #bytr = binascii.hexlify(f.readline(8))
+            #print("File position: %s" % str(f.tell()))
+            #print("Last 8 bytes of file: %s" % str(bytr))
+#
+            #if bytr == '0000000000000000':
+                #print("BNSF file is too long!")
+                #sizeb = os.path.getsize(fileO)
+                #print("Full size of file in bytes: %s" % str(sizeb))
+                #sizeE = sizeb - 8
+                #print("Size of file - 8 bytes: %s" % str(sizeE))
+                #f.truncate(sizeE)
+                #print("BNSF file truncated to proper length.\n")
+            #else:
+                #print("BNSF file should work!\n")
+#
+            #f.close()
+
+try:            
+    done = input("Truncation complete. Press any key to exit.")
+except EOFError:
+    done = "\n"
+finally:
+    sys.exit()
